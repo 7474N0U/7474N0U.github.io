24e921601d8e3603fa95d943b23774dc5eaa3164
Charly <unsky.dev@gmail.com>
2025-04-04 22:50:20 +0200

    Mise à jour du style des widgets météo avec une bordure et une ombre modifiées, et correction des icônes pour les conditions météorologiques

--- assets/js/meteo.js
+++ assets/js/meteo.js
@@ -159,13 +159,13 @@ async function createWeatherWidget(city) {
         const weatherMap = {
             0: { label: "Ensoleillé", icon: "sun_sunny.svg", class: "sunny" },
             1: { label: "Peu nuageux", icon: "sun_cloudy.svg", class: "cloudy" },
-            2: { label: "Nuageux", icon: "cloud.svg", class: "cloudy" },
+            2: { label: "Nuageux", icon: "sun_cloudy.svg", class: "cloudy" },
             3: { label: "Très nuageux", icon: "cloud.svg", class: "cloudy" },
             45: { label: "Brouillard", icon: "fog.svg", class: "foggy" },
             51: { label: "Bruine", icon: "rain.svg", class: "rainy" },
             61: { label: "Pluie", icon: "rain.svg", class: "rainy" },
-            80: { label: "Averses", icon: "rain.svg", class: "rainy" },
-            95: { label: "Orage", icon: "thunder.svg", class: "stormy" }
+            80: { label: "Averses", icon: "shower.svg", class: "rainy" },
+            95: { label: "Orage", icon: "sun_stormy.svg", class: "stormy" }
         };
 
         const weatherInfo = weatherMap[weatherCode] || { label: "Inconnu", icon: "sun_sunny.svg", class: "sunny" };
